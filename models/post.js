61e461717c3a859140aaaba21d150293a1eefdad
itspranit <pranitrathkanthiwar123@gmail.com>
2025-07-05 17:44:18 +0530

    add comments to the db

--- models/post.js
+++ models/post.js
@@ -8,9 +8,19 @@ const postSchema=new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
-    }
+    },
+    //include the array of ids of all coments in this post schema itself
+    comments:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'comment'
+    }]
 },{
    timestamp:true 
 });
 const Post=mongoose.model('Post',postSchema);
 module.exports=Post;
+
+//Note:we could have included post object inside users like
+// we added comments objet inside post object but usecase of 
+//this is rare like we very few times actually go to a user
+// and see their post while comments are always displayed along side the post,which makes it faster
